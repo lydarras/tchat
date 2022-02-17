e4761149301363d0d1524d25e59b05551b336969
Darras <lydarras@gmail.com>
2022-02-17 13:33:24 +0100

    ajout une sécurité de la messagerie (champ vide)

--- createMessage.php
+++ createMessage.php
@@ -1,23 +1,43 @@
 <?php
+session_start();
+echo "Pseudo : ".$_POST['pseudo']."<br/>";
+echo "Message ".$_POST['message']."<br/>";
+echo strlen(trim($_POST['message']));
+
+//condition pour assurer si les saisies sont vides ou non
+if(!empty($_POST['pseudo']) && strlen(trim($_POST['message'])) != 0){
 
 //Connexion de la BDD
-require_once("connexionPDO.php");
+    require_once("connexionPDO.php");
 
-//La reqûete SQL pour insérer
-$requete = "INSERT INTO message(id_message, pseudo, message, date_enregistrement) VALUES (NULL,:pseudo,:message,NOW())";
+    //La reqûete SQL pour insérer
+    $requete = "INSERT INTO message(id_message, pseudo, message, date_enregistrement) VALUES (NULL,:pseudo,:message,NOW())";
 
-//Le statement permet d'abord de préparer la reqûete
-//le : est un marqueur nominatif pour assigner la variable
-$stmt = $pdo->prepare($requete);
+    //Le statement permet d'abord de préparer la reqûete
+    //le : est un marqueur nominatif pour assigner la variable
+    $stmt = $pdo->prepare($requete);
 
-//le bindParam permet d'affecter le marqueur nominatif, les valeurs POST qui sont censés d'être saisies
-$stmt->bindParam(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
-$stmt->bindParam(':message',$_POST['message'], PDO::PARAM_STR);
+    //le bindParam permet d'affecter le marqueur nominatif, les valeurs POST qui sont censés d'être saisies
+    $stmt->bindParam(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
+    $stmt->bindParam(':message',$_POST['message'], PDO::PARAM_STR);
 
-//Une fois affecté les, la reqûete d'insertion sera executé
-$stmt->execute();
+    //Une fois affecté les, la reqûete d'insertion sera executé
+    $stmt->execute();
 
-//Le header permet de rediriger vers la page index et en théorie, le message est inséré
-header('Location: index.php');
+    //Si l'utilisateur n'a fait aucun saisi précédemment ou sur l'un des deux
+    //Dans le cas où toutes les conditions sont remplies, on peur retirer le message d'alerte
+    if(isset($_SESSION['alerte'])){
+        unset($_SESSION['alerte']);
+    }
+
+    //Le header permet de rediriger vers la page index et en théorie, le message est inséré
+    header('Location: index.php');
+}
+else{
+    //fera affiche le message  alerte quand on sera redirigé vers l'index
+    $_SESSION['alerte'] = "Priez de remplir toutes les champs !<br/>";
+    header('Location: index.php');
+}
+
 
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,18 @@
 </head>
 <body>
     <div class="messagerie">
-    <?php require_once("showMessage.php") ?>
+    <?php
+    //La session sera utile pour afficher les erreurs
+    session_start();
+
+    //Dans le cas où y a une valeur dans l'index alerte
+    //On fera afficher le message
+    if(isset($_SESSION['alerte']))
+        echo $_SESSION['alerte'];
+
+    //Charge le fichier showMessage qui consiste à faire afficher les messages
+    require_once("showMessage.php");
+    ?>
     </div>
 
     <div id="formulaire">
@@ -17,8 +28,7 @@
         <label for="pseudo">Pseudo:</label>
         <input type="text" id="pseudo" name="pseudo">
         <label for="message">Votre message:</label>
-        <textarea id="message" name="message">
-        </textarea>
+        <textarea id="message" name="message"></textarea>
         <input type="submit" value="Valider">
     </form>
     </div>
